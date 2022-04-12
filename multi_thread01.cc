e8cdc99494c774084d383c90bc151b16084e4c57
Jerry-1015 <781467164@qq.com>
2022-04-12 15:36:24 +0800

    master

--- multi_thread01.cc
+++ multi_thread01.cc
@@ -1,16 +1,32 @@
 #include <iostream>
 #include <thread>
-using namespace std;
 
 void func(int a) {
-    cout << "hello world!" << endl;
-    this_thread::sleep_for(chrono::milliseconds(50));
+     {
+        // std::cout << "hello world!" << std::endl;
+        std::this_thread::sleep_for(std::chrono::milliseconds(50));
+    
+    }
 }
 
 int main() {
     int a = 0;
-    cout << "hello" << endl;
-    thread thread1(func, a);
+    std::thread thread1(func, a);
 
-    while (true);
+
+    std::cout << "the thread id is :" << thread1.get_id() << std::endl;
+    std::cout << "support num is " << thread1.hardware_concurrency();
+    // 第一种方法  子线程执行完毕才会执行下面代码
+
+    thread1.join(); 
+    
+
+    // 第二种方法  两个线程分离开 尽量少用 保证没有悬空指针
+    // thread1.detach();
+
+    // while (true) {
+    //     std::cout << "Cat !\n";
+    //     std::this_thread::sleep_for(std::chrono::milliseconds(50));
+    // }
+    //while (true);
 }
